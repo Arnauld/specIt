c10d8b17652ede23bed38bb4555e1962e104e75e
Arnauld Loyer <arnauld.loyer@gmail.com>
2012-07-18 19:15:49 +0200

    Add docco based documentation + customize template

--- src/test/java/specit/sample/calculator/CalculatorSampleTest.java
+++ src/test/java/specit/sample/calculator/CalculatorSampleTest.java
@@ -32,56 +32,87 @@ public class CalculatorSampleTest {
 
     /*!
        Next one has to provide the settings for our tests.
+       This annotated method is required by the SpecIt Junit Runner, it provides
+       the configuration required to run our test: the stories to execute, the
+       glue to use to bridge the steps with the actual code.
      */
     @SpecItRunner.Parameters
     public static SpecIt conf() throws ParameterMappingException {
         return new SpecIt()
                 /*!
-                    Let's define our own aliases.
+                   Let's define our own aliases.
+
+                   Whereas this is not necessary because all those aliases are already predefined,
+                   it shows how simple it is to add your own alias for a specific keyword.
+                 */
+                /*!
+                   An example would be for context specification, it is possible to express story like:
+
+                     When a product is added that is not already in the cart
+                      - The cart item factory should be used to create a cart item for the product being added.
+                     When an item is added
+                      - The item count should be incremented
+                      - The item should be added to the underlying list
+
+                   by simply define `-` as an alias for the Keyword `Then`.
+
+                     specIt.withAlias(Keyword.Then, "-")
+
+                   Alias are used when scanning step to figure out what kind of the step it belongs to,
+                   technically it is based on the starting words of a new line. Thus if a line starts
+                   with `-` (trailing whitespaces are _usually_ ignored) it will be considered as a `Then`
+                   step.
                  */
                 .withAlias(Keyword.Scenario, "Scenario:")
                 .withAlias(Keyword.Given, "Given")
                 .withAlias(Keyword.When, "When")
                 .withAlias(Keyword.Then, "Then")
-                        /*!
-                           **This is one of the most important part**. It indicates to SpecIt
-                           where to find the glue used for bridging story to code.
-                        */
+                /*!
+                   **This is one of the most important part**. It indicates to SpecIt
+                   where to find the glue used for bridging story to code.
+                   The `Steps` class is defined above, it contains annotated methods that
+                   must match the steps used in the story. Each step must match one annotated
+                   method.
+                   In this case, only one class is being parsed, but it is possible to
+                   use any number of annotated classes.
+                */
                 .scanAnnotations(Steps.class)
-                        /*!
-                           One indicates the stories that should be executed.
-                           The story paths provided are used as key for the StoryLoader defined. Thus it may
-                           be anything as long as the corresponding StoryLoader knows how to handle it.
-                           **Usually they simply refer to resource or file names.**
-
-                        */
-                .withStoryPaths("simple_additions")
-                        /*!
-                           The Story loader used. In this cas, one create a basic one.
-                           Creating a `StoryLoader` is a really simple thing to do. It may be usefull
-                           to write your own if the default ones doesn't fulfills your needs.
-                           e.g. load stories from a remote location or from a database...
-                        */
+                /*!
+                   One indicates the stories that should be executed.
+                   The story paths are used as key for the `StoryLoader`. Thus it may
+                   be anything as long as the corresponding `StoryLoader` knows how to handle it.
+                   **Usually they simply refer to resource or file names.**
+                */
+                .withStoryPaths("simple_additions.story")
+                /*!
+                   The Story loader used. In this cas, one create a basic one.
+                   Creating a `StoryLoader` is a really simple thing to do. It may be usefull
+                   to write your own if the default ones doesn't fulfills your needs.
+                   e.g. load stories from a remote location or from a database...
+                */
                 .withStoryLoader(createStoryLoader())
-                        /*!
-                           Defines a Simple Console output. If launched from outside an IDE (eclipse or intellij)
-                           output will be colored using ANSI patterns.
-                        */
+                /*!
+                   Defines a Simple Console output.
+
+                   If launched from outside an IDE (eclipse or intellij)
+                   output will be colored using ANSI patterns.
+                */
                 .withReporter(new ConsoleColoredReporter(!IDE.isExecutedWithinIDE()))
                 ;
     }
 
     /*!
-        A build-in story loader that will provides our stories directly from string.
+       A build-in story loader that will provides our stories directly from string.
+     */
+    /*!
+       <small>
+       Whereas this is not a usual case, and `specit.support.ResourceStoryLoader` should be
+       preferred, one uses this approach to show how simple it is to write a `StoryLoader`,
+       and to keep all the data in this single class.
+       </small>
 
-        <small>
-        Whereas this is not a usual case, and `specit.support.ResourceStoryLoader` should be
-        preferred, one uses this approach to show how simple it is to write a `StoryLoader`,
-        and to keep all the data in this single class.
-        </small>
-
-        There are predefined implementation such as `ResourceStoryLoader` that allows
-        to load story based on resource classpath.
+       There are predefined implementation such as `ResourceStoryLoader` that allows
+       to load story based on resource classpath.
      */
 
     /**
@@ -92,23 +123,46 @@ public class CalculatorSampleTest {
             @Override
             public String loadStoryAsText(String storyPath) {
                 /*!
-                    When the story path is `simple_additions`.
+                   When the story path is `simple_additions`.
+
+                   This is roughly equivalent to store the content in a file `simple_addition.story`
+
+                    Scenario: 2+2
+
+                      Given a variable x with value 2
+                       When I add 2 to x
+                       Then x should equal to 4
+
+                   and to use the `ResourceStoryLoader` instead of our custom one.
+
                  */
-                if ("simple_additions".equals(storyPath)) {
+                if ("simple_additions.story".equals(storyPath)) {
                     return "Scenario: 2+2\n" +
                             "\n" +
                             "  Given a variable x with value 2\n" +
                             "   When I add 2 to x\n" +
                             "   Then x should equal to 4";
                 }
+
+
+                /*!
+                   Let's be a bit paranoid ;)
+                 */
                 throw new RuntimeException("No story found at path <" + storyPath + ">");
             }
         };
     }
 
-    /*! Let's define the Calculator that will be tested.
-       It is a simple calculator that provides three methods to define a new variable,
-       add a given value to a known variable and retrieve the value of a variable.
+    /*!
+       Let's define the Calculator that will be tested.
+
+       Actually this will be the class that will be under test.
+
+       It is a simple calculator that provides three methods:
+
+       * define a new variable,
+       * add a given value to a known variable,
+       * retrieve the value of a variable
     */
     public static class Calculator {
         private Map<String, Integer> variables = New.hashMap();
@@ -131,8 +185,8 @@ public class CalculatorSampleTest {
     }
 
     /*!
-        Once our story is defined. Let's now write the glue between the story and the actual code that
-        is instrumented through our steps.
+       Once our story is defined. Let's now write the glue between the story and the actual code that
+       is instrumented through our steps.
      */
     public static class Steps {
 
@@ -162,24 +216,24 @@ public class CalculatorSampleTest {
         @Given("a variable $variable with value $value")
         public void defineVariable(
                 /*!
-                    Provides the calculator currently defined in the context.
-                    The calculator has automatically been created by the
-                    `calculator()` method
+                   Provides the calculator currently defined in the context.
+                   The calculator has automatically been created by the
+                   `calculator()` method
                  */
                 @UserContext Calculator calculator,
                 /*!
-                    Provides the name of the variable, extracted from the step.
-                    The value will be retrieved from the `$variable` value.
-                    Note that the variable name and the parameter name does not have
-                    to match. By default, the variables are provided in the order they are defined,
-                    otherwise one must indicates by annotating each parameter with the name of
-                    the variable it refers to.
+                   Provides the name of the variable, extracted from the step.
+                   The value will be retrieved from the `$variable` value.
+                   Note that the variable name and the parameter name does not have
+                   to match. By default, the variables are provided in the order they are defined,
+                   otherwise one must indicates by annotating each parameter with the name of
+                   the variable it refers to.
                  */
                 String variableName,
                 /*!
-                    Provides the expected value of the variable.
-                    Default converters handle the conversion of String to `int`, enum value (based on their
-                    name) and `Table` for tabular parameters.
+                   Provides the expected value of the variable.
+                   Default converters handle the conversion of String to `int`, enum value (based on their
+                   name) and `Table` for tabular parameters.
                  */
                 int value)
         {
@@ -195,8 +249,8 @@ public class CalculatorSampleTest {
         }
 
         /*!
-            A `Then` is the place to check the result: almost all assertions should be
-            placed within a `Then`.
+           A `Then` is the place to check the result: almost all assertions should be
+           placed within a `Then`.
          */
         @Then("$variable should equal to $value")
         public void assertVariableIsEqualTo(
@@ -209,12 +263,12 @@ public class CalculatorSampleTest {
                  */
                 String variableName,
                 /*!
-                    Provides the expected value of the variable.
+                   Provides the expected value of the variable.
                 */
                 int expectedValue)
         {
             /*!
-                One relies on FestAssert to ease our assertion.
+               One relies on FestAssert to ease our assertion.
              */
             assertThat(calculator.getVariableValue(variableName)).isEqualTo(expectedValue);
         }
